a6459fbdcc2f5792a7f639f4e679d0a79f8345ed
Haimmichaeli90 <haimmichaeli95@gmail.com>
2025-05-04 10:12:23 +0300

    adding option to origin

--- back-end/server.js
+++ back-end/server.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 5001
 const corsOptions = {
     origin: [
         'http://127.0.0.1:5173',
-        'http://localhost:5173'
+        'http://localhost:5173',
+        'https://newsitep-2.onrender.com'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
